9233206bea4c4f4e9ef64157672439a40cf77ec6
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2020-01-16 00:52:15 +0100

    added decorator

---
+++ myutils.py
@@ -0,0 +1,18 @@
+
+def print_invocation(f):
+    def wrapper(*args, **kwargs):
+        print(f"{f.__name__} called")
+        ret = f(*args, **kwargs)
+        print(f"{f.__name__} returned {ret}")
+        return ret
+    return wrapper
+
+def dump_args(f):
+	argnames = f.__code__.co_varnames
+	def wrapper(*args, **kwargs):
+		argval = ','.join('%s=%r' % entry for entry in zip(argnames, args) )
+		print(f"{f.__name__}({argval})")
+		return f(*args, **kwargs)
+	return wrapper
+
+
