2c6af7d74dd475ad5010455da36207c254fbd32a
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2020-01-20 18:59:59 +0100

    refactor

---
+++ svr/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from myutils import *
+import numpy as np
+from SLBQP import *
+from SVR import SVR
+import pickle
+
+
+
+
+if __name__ == '__main__':
+    args = get_cmdline_args()
+    X, Y1, Y2 = load_data(args.file) 
+    print("data loaded...")   
+    regressor = SVR(maxIter=3000, eps=1e-4)
+    print("start fitting...")
+
+    Yscaled, M, m = scale(Y2)
+    if args.percentage is not None:
+    	X, V = splitHorizontally(X, float(args.percentage))
+    	Y, YV = splitHorizontally(Yscaled, float(args.percentage))
+
+    regressor.fit(X, Y)
