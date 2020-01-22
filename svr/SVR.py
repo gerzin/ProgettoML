90a4ed3904cb7c938a374780102a7defc6eb216e
marcomerton <leprimarco97@gmail.com>
2020-01-22 12:04:34 +0100

    rearrangements

--- svr/SVR.py
+++ svr/SVR.py
@@ -4,17 +4,8 @@ from SLBQP import SLBQP
 from numba import jit, jitclass, int32, float32, float64
 import pickle
 
-def linear(x,y):
-    return np.dot(x,y)
-
 @jit(nopython=True)
-def rbf(x,y, gamma=1):
-    a = np.dot(x-y, x-y)
-    return np.exp(-gamma*a)
-
-
-@jit(nopython=True)
-def compute_K_matrix(dataset, gamma=1):
+def compute_rbf_matrix(dataset, gamma=1):
     n = len(dataset)
     K = np.empty((n,n), np.float64)
     for i in range(n):
@@ -23,7 +14,6 @@ def compute_K_matrix(dataset, gamma=1):
             diff = dataset[i] - dataset[j]
             a = np.dot(diff, diff)
             v = np.exp(-gamma*a)
-            #v = rbf(dataset[i], dataset[j])
             K[i][j] = v
             K[j][i] = v
     return K
@@ -37,8 +27,8 @@ class SVR:
     """
     Support Vector Regressor
     """
-    def __init__(self, ker=rbf, gamma=1, C=1, eps=0.1, tol=1e-3, maxIter=1000):
-        self.ker = rbf
+    def __init__(self, ker='rbf', gamma=1, C=1, eps=0.1, tol=1e-3, maxIter=1000):
+        self.ker = ker
         self.gamma = gamma
         self.C = C
         self.eps = eps
@@ -49,13 +39,14 @@ class SVR:
         self.bias = 0
     
     def fit(self,X, y):
-        #K = self._compute_kernel_matrix(X, self.ker)
-        K = compute_K_matrix(X)
+        K = compute_rbf_matrix(X, self.gamma)
         Q, q, a = self._prepare(K, y)
+
         x = np.full(len(q), self.C/2)
         status, x, e = SLBQP(Q, q, self.C, a, x, self.tol, self.maxIter)
         if status == 'terminated':
             print("maxIter reached")
+
         self.data = X
         self.gammas = self._compute_gammas(x)
         self.bias = self._compute_bias(y)
@@ -65,19 +56,6 @@ class SVR:
         for i in range(len(self.data)):
             d+= self.gammas[i]*self.ker(pattern,self.data[i])
         return d
-
-    def _compute_kernel_matrix(self,dataset, dot_product):
-        n = len(dataset)
-        K = np.empty([n,n])
-        
-        for i in range(n):
-            for j in range(i,n):
-                v = dot_product(dataset[i], dataset[j])
-                
-                K[i][j] = v
-                K[j][i] = v
-
-        return K
 
     def _prepare(self, K, d):
         (n,m) = K.shape
@@ -108,6 +86,29 @@ class SVR:
         a = x[:n]
         a1 = x[n:]
         return a-a1
+    
+    def _compute_bias(self, y):
+        cont = 0
+        bias = 0
+        for i in range(len(self.gammas)):
+            gamma = self.gammas[i]
+            if 0< gamma < self.C:
+                cont += 1
+                bias += y[i] - self.predict(self.data[i])
+        return bias/cont
+    
+    def evaluateMSE(self, patterns, targets):
+        outputs = [self.predict(x) for x in patterns]
+        return MSE(outputs, targets)
+    
+    
+    def __repr__(self):
+        s = "SVR:\n"
+        paramNames = ["kernel", "gamma", "C", "eps", "tol", "maxIter", "data", "gammas", "bias"]
+        infos = [str(x) for x in \
+                    [self.ker,self.gamma, self.C, self.eps, self.tol, self.maxIter, self.data, self.gammas, self.bias]]
+        
+        return s + "\n".join([f"{a}={b}" for a,b in zip(paramNames, infos)])
 
     def save(self, filename):
         """Save the regressor parameters on a file"""
@@ -120,34 +121,3 @@ class SVR:
         with open(filename, "rb") as filename:
             r = pickle.load(filename)
         return r
-    
-    def _compute_bias(self, y):
-        cont = 0
-        bias = 0
-        for i in range(len(self.gammas)):
-            gamma = self.gammas[i]
-            if 0< gamma < self.C:
-                cont += 1
-                bias += y[i] - self.predict(self.data[i])
-        
-        return bias/cont
-    
-    def __repr__(self):
-        s = "SVR:\n"
-        paramNames = ["kernel", "gamma", "C", "eps", "tol", "maxIter", "data", "gammas", "bias"]
-        infos = [str(x) for x in \
-                    [self.ker.__name__,self.gamma, self.C, self.eps, self.tol, self.maxIter, self.data, self.gammas, self.bias]]
-        
-        return s + "\n".join([f"{a}={b}" for a,b in zip(paramNames, infos)])
-
-    def evaluateMSE(self, patterns, targets):
-        outputs = [self.predict(x) for x in patterns]
-        return MSE(outputs, targets)
-        
-      
-
-
-
-
-
-
