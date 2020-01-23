87516686e463b1c81f30e85cb4da24c47557d672
marcomerton <leprimarco97@gmail.com>
2020-01-23 09:40:17 +0100

    testing

--- svr/main.py
+++ svr/main.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
 from myutils import *
 import numpy as np
-from SLBQP import *
-from SVR import SVR
-import pickle
-
+from SVR import SVR as svr1
+from sklearn.svm import SVR as svr2
 
 
 
 if __name__ == '__main__':
     args = get_cmdline_args()
     X, Y1, Y2 = load_data(args.file) 
-    print("data loaded...")   
-    regressor = SVR(maxIter=3000, eps=1e-4)
-    print("start fitting...")
-
-    Yscaled, M, m = scale(Y2)
-    if args.percentage is not None:
-    	X, V = splitHorizontally(X, args.percentage)
-    	Y, YV = splitHorizontally(Yscaled, args.percentage)
-
-    regressor.fit(X, Y)
+    reg = svr2()
+    reg.fit(X,Y1)
+    indeces = reg.support_
+    s1 = reg.dual_coef_
+    
+    
+    g = 1/(len(X[0])*X.var())
+    r = svr1(gamma=g, maxIter=5000)
+    r.fit(X,Y1)
+    
+    v = ((Y1 - Y1.mean())**2).sum()
+    YP = [r.predict(i) for i in X]
+    u = ((Y1 - YP)**2).sum()
+    s2 = r.gammas
+    
+    i = 0
+    for j in indeces:
+        print(f"{s1[0][i]} = {s2[j]}")
+        i += 1
+        
+    print(1 - u/v)
+    print(reg.score(X,Y1))
