3c4a626af4d9322a7f5b123c2b620778461fd7e2
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2020-01-23 15:35:46 +0100

    added grid_search

---
+++ svr/grid_search.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import numpy as np
+from numba import njit
+from myutils import *
+from inspect import signature
+from SVR import *
+import itertools as it
+from model_selection import *
+
+class GridSearcher:
+    def __init__(self, reg, ranges=None):
+        param_names = [p for p in signature(reg.__init__).parameters]
+        param_names = param_names[2:]
+        self.grid = None
+        if len(param_names) != len(ranges):
+            raise "Wrong number of ranges"
+        self.param_ranges = zip(param_names, ranges)
+        self.ranges = ranges
+        print(*self.param_ranges)
+        for a,b in self.param_ranges:
+            print(f"{a} {b}")
+        self._generate_grid()
+    
+    def _generate_grid(self):
+        print("generating grid...")
+        self.grid = it.product(*self.ranges)
+
+    def start_search(self, X, Y, k):
+        print("starting grid search...")
+        ind = k_fold_split_indices(X,Y,k)
+        for i in self.grid:
+            gamma, C, eps, tol, maxIter = i
+            err = k_fold_evaluate(X, Y, k, ind, gamma, C, eps, maxIter)
+
+if __name__ == '__main__':
+    
+    args = get_cmdline_args()
+    print("loading data...")
+    X, Y1, Y2 = load_data(args.file)
+    Y, M, m = None, None, None
+
+    if args.column == 1:
+        Y = Y1
+    else:
+        Y = Y2
+
+    if args.scale:
+        print("scaling data...")
+        Y, M, m = scale(Y) 
+    
+    gamma = [10**i for i in range(-3, 2)]
+    C = [10**i for i in range(-1, 4)]
+    eps = [i for i in np.linspace(0.01, 1, num=5)]
+    tol = [1e-3]
+    maxiter = [5000]
+    ranges = (gamma, C, eps, tol, maxiter)
+    gs = GridSearcher(SVR, ranges)
+    gs.start_search(X,Y,args.kfold)
+
