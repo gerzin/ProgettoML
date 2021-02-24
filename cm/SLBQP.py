d967936b34084cc08f3457bc06bd7341a2e1e230
marcomerton <leprimarco97@gmail.com>
2021-02-24 17:19:26 +0100

    renamed file

--- cm/SLBQP.py
+++ cm/SLBQP.py
@@ -1,65 +1,62 @@
+#!/usr/bin/env python3
+
 import numpy as np
-from numba import jit, njit
+#from numba import jit, njit
 
 """
 SLBQP: Solves the singly linearly box contrained quadratic problem:
 
-    min { (1/2)x'Qx + qx : ax = 0, 0 <= x <= C }
+    min { (1/2)x'Qx + qx : ax = 0, 0 <= x <= u }
 
-with
-Q = [ K -K
-     -K  K ]
-q = [epsilon - y, epsilon + y] and
-a = [1 ... 1, -1 ... -1]
 using the projected gradient method.
 
+For the projection, two options are available:
 
-The projection is computed with the algorithm described in [?]:
+1. Project the point, found by following the minimization direction, onto the feasible region.
+    The projection is computed with the algorithm described in [?]:
     
-The projection is the solution of the minimization problem:
-    min { (1/2)z'z - d'z : 0 <= z <= u, a'z = 0 }
+    The projection is the solution of the minimization problem:
+        min { (1/2)z'z - d'z : 0 <= z <= u, a'z = 0 }
 
-It can be solved using the Lagrangian function:
-    ϕ(x,λ) = (1/2)z'z - d'z - λa'z
-For a given λ we may solve:
-    min { ϕ(x) : 0 <= z <= u }
-This is trivial as the problem separates into n independent subproblems.
+    It can be solved using the Lagrangian function:
+        ϕ(x,λ) = (1/2)z'z - d'z - λa'z
+    For a given λ we may solve:
+        min { ϕ(x) : 0 <= z <= u }
+    This is trivial as the problem separates into n independent subproblems.
 
-The point is to find a λ such that r(λ) = a'x(λ) = 0 in order to satisfy the KKT
-conditions for the initial minimization problem.
-To do so, we look for the root of r(λ) using the secand method.
-Therefor, we first look for an interval containing the solution (BRACKETING PHASE)
-and then we look for the root (SECANT PHASE)
+    The point is to find a λ such that r(λ) = a'x(λ) = 0 in order to satisfy the KKT
+    conditions for the initial minimization problem.
+    To do so, we look for the root of r(λ) using the secand method.
+    Therefor, we first look for an interval containing the solution (BRACKETING PHASE)
+    and then we look for the root (SECANT PHASE)
+
+2. Prohect the gradient so that, moving along its direction, the iteration point remains feasible.
+    ...
+
 """
 
-@jit(nopython=True, parallel=True)
-def compute_x_r(d1, d2, lmb, u, n):
+#@jit(nopython=True, parallel=True)
+def compute_x_r(d1, d2, lmb, u):
     """Compute the optimal value for x given lambda.
     Params:
         d1  -- first block of the direction vector
         d2  -- second block of the direction vector
-        lmb --
+        lmb -- current value for lambda
         u   -- upper bound of the feasible region
-        n   -- dimension of the problem <- TODO: n inutile. rimuovere
     Returns:
-        x1  --
-        x2  --
-        r   --
+        x1  -- first block of the optimal x
+        x2  -- second block of the optimal x
+        r   -- value of r given the optimal x and lambda
     """
     
     # x = d + lmb * a
     # with a = [1...1,-1...-1]
     x1 = d1 + lmb
     x2 = d2 - lmb
-    
+
     # 'Apply' the box constraints
-    for i in range(n):
-        if(x1[i] > u): x1[i] = u
-        elif(x1[i] < 0): x1[i] = 0
-        
-    for i in range(n):
-        if(x2[i] > u): x2[i] = u
-        elif(x2[i] < 0): x2[i] = 0
+    x1 = np.clip(x1, 0, u)
+    x2 = np.clip(x2, 0, u)
        
     # r = a'x
     # with a = [1...1,-1...-1]
@@ -67,10 +64,11 @@ def compute_x_r(d1, d2, lmb, u, n):
     
     return x1, x2, r
 
-@jit(nopython=True)
-def project(d1, d2, u, lmb, d_lmb, eps):
+#@jit(nopython=True)
+def project_Goldstein(d1, d2, u, lmb, d_lmb, eps):
     """ Return the projection of the point d over the feasible region
     defined by 0 <= x <= u and ax = 0 with a = [1...1,-1...-1]
+
     Params:
         d1      -- first block of the direction vector
         d2      -- second block of the direction vector
@@ -78,15 +76,17 @@ def project(d1, d2, u, lmb, d_lmb, eps):
         lmb     -- 
         d_lmb   -- delta lambda
         eps     -- precision
-    Returns:
 
+    Returns:
+        x1      -- first block of the projected point
+        x2      -- second block of the projected point
     """
-    n = len(d1)
+
     # BRACKETING PHASE -----
     # ----------------------
     
     # Compute x and r and check whether it found the minimum
-    x1, x2, r = compute_x_r(d1, d2, lmb, u, n)
+    x1, x2, r = compute_x_r(d1, d2, lmb, u)
     if abs(r) < eps:
         return x1, x2
     
@@ -94,10 +94,10 @@ def project(d1, d2, u, lmb, d_lmb, eps):
         # r(λ) < 0 -> search for a λ | r(λ) > 0
                 
         # initialize lower bounds and update
-        lmb_l = lmb
-        r_l = r
+        lmb_l = lmb;
+        r_l = r;
         lmb += d_lmb
-        x1, x2, r = compute_x_r(d1, d2, lmb, u, n)
+        x1, x2, r = compute_x_r(d1, d2, lmb, u)
         if abs(r) < eps:
             return x1, x2
         
@@ -108,7 +108,7 @@ def project(d1, d2, u, lmb, d_lmb, eps):
             s = max(r_l/r -1, 0.1); d_lmb += d_lmb/s; lmb += d_lmb
             
             # Compute x and r and check whether it found the minimum
-            x1, x2, r = compute_x_r(d1, d2, lmb, u, n)
+            x1, x2, r = compute_x_r(d1, d2, lmb, u)
             if abs(r) < eps:
                 return x1, x2
         
@@ -125,7 +125,7 @@ def project(d1, d2, u, lmb, d_lmb, eps):
         lmb -= d_lmb
         
         # Compute x and r and check whether it found the minimum
-        x1, x2, r = compute_x_r(d1, d2, lmb, u, n)
+        x1, x2, r = compute_x_r(d1, d2, lmb, u)
         if abs(r) < eps:
             return x1, x2
         
@@ -137,7 +137,7 @@ def project(d1, d2, u, lmb, d_lmb, eps):
             lmb -= d_lmb
             
             # Compute x and r and check whether it found the minimum
-            x1, x2, r = compute_x_r(d1, d2, lmb, u, n)
+            x1, x2, r = compute_x_r(d1, d2, lmb, u)
             if abs(r) < eps:
                 return x1, x2
         
@@ -148,7 +148,7 @@ def project(d1, d2, u, lmb, d_lmb, eps):
     s = 1 - r_l/r_u
     d_lmb = d_lmb/s
     lmb = lmb_u - d_lmb
-    x1, x2, r = compute_x_r(d1, d2, lmb, u, n)
+    x1, x2, r = compute_x_r(d1, d2, lmb, u)
 
     while(abs(r) >= eps):
         if(r > 0):
@@ -174,12 +174,12 @@ def project(d1, d2, u, lmb, d_lmb, eps):
                 lmb_l = lmb; r_l = r; lmb = lmb_new
                 s = (lmb_u - lmb_l)/(lmb_u - lmb)
 
-        x1, x2, r = compute_x_r(d1, d2, lmb, u, n)
+        x1, x2, r = compute_x_r(d1, d2, lmb, u)
                 
     return x1, x2
 
 #@njit
-def project1(d1, d2, x1, x2, u, n):
+def project_Rosen(d1, d2, x1, x2, u, n):
     """ Rosen projection of d over the feasible region 0 <= x <= u
 
     Params:
@@ -189,93 +189,56 @@ def project1(d1, d2, x1, x2, u, n):
         x2  -- second block of the iterate
         u   -- upper bound of the feasible region
         n   -- dimension of the problem
+
     Returns:
-        cambiare nome a temp
+        proj1  -- first block of the projected gradient
+        proj2  -- second block of the projected gradient
     """
+
     active_indeces1 = [(x1[i] == 0 and d1[i] < 0) or (x1[i] == u and d1[i] > 0) for i in range(n)]
     active_indeces2 = [(x2[i] == 0 and d2[i] < 0) or (x2[i] == u and d2[i] > 0) for i in range(n)]
-    
-    temp1 = None
-    temp2 = None
-    
-    test = 0
-    while(test != -1):
-        d_sum = 0
-        temp1 = np.array(d1)
-        for i in range(n):
-            if(active_indeces1[i]):
-                temp1[i] = 0
-            else:
-                d_sum += d1[i]
 
-        temp2 = np.array(d2)
-        for i in range(n):
-            if(active_indeces2[i]):
-                temp2[i] = 0
-            else:
-                d_sum -= d2[i]
+    free_indeces1 = [not i for i in active_indeces1]
+    free_indeces2 = [not i for i in active_indeces2]
+    
+    changed = True
+    while(changed):
 
+        proj1 = np.zeros(n)
+        proj2 = np.zeros(n)
+
+        d_sum = sum(d1[free_indeces1]) - sum(d2[free_indeces2])
         den = (2*n - sum(active_indeces1) - sum(active_indeces2))
+
         if(den == 0):
-            return np.zeros(n), np.zeros(n)
+            return proj1, proj2
         else:
-            v = d_sum/den
-        #print(f"{den} ", end='')
-            
-        #v = d_sum/(2*n - sum(active_indeces1) - sum(active_indeces2))
-        for i in range(n):
-            if not active_indeces1[i]:
-                temp1[i] = d1[i] - v
-        for i in range(n):
-            if not active_indeces2[i]:
-                temp2[i] = d2[i] + v
+            v = d_sum / den
+
+        proj1[free_indeces1] = d1[free_indeces1] - v
+        proj2[free_indeces2] = d2[free_indeces2] + v
     
-        test = -1
+        changed = False
         for i in range(n):
-            if (x1[i] == 0 and temp1[i] < 0) or (x1[i] == u and temp1[i] > 0):
+            if (x1[i] == 0 and proj1[i] < 0) or (x1[i] == u and proj1[i] > 0):
                 active_indeces1[i] = True
-                test = i
+                changed = True
                 break
     
-        if (test != -1):
-            continue
+        if (changed): continue
 
         for i in range(n):
-            if (x2[i] == 0 and temp2[i] < 0) or (x2[i] == u and temp2[i] > 0):
+            if (x2[i] == 0 and proj2[i] < 0) or (x2[i] == u and proj2[i] > 0):
                 active_indeces2[i] = True
-                test = i
+                changed = True
                 break
     
-    return temp1,temp2
-
-@njit
-def equal(x, b, tol):
-    return np.abs(x-b) < tol
-@njit
-def active_set(x, u, tol=1e-6):
-    """
-    Retrun arrays containing indices in the active set.
-    """
-    Zero = [ i for (i,e) in enumerate(equal(x,0,tol)) if e ]
-    U = [ j for (j,e) in enumerate(equal(x,u, tol)) if e ]
-    return set(Zero), set(U)
-
-
-def active_set_changes(Z_old, U_old, Z, U):
-    U_in = U - U_old
-    U_out = U_old - U
-    
-    Z_in = Z - Z_old
-    Z_out = Z_old - Z
-    
-    return (len(Z_in) > 0 or len(Z_out) > 0), (len(U_in) > 0 or len(U_out) > 0)
-
+    return proj1, proj2
 
 
 def SLBQP(K, y, C, epsilon, eps=1e-6, maxIter=1000, lmb0=0, d_lmb=2, prj_eps=1e-6, verbose=False, prj_type=1):
     """
     Solve the quadratic problem using a projected gradient method.
-
 
     Params:
         K           -- block of the matrix Q =  [ K -K
@@ -284,12 +247,13 @@ def SLBQP(K, y, C, epsilon, eps=1e-6, maxIter=1000, lmb0=0, d_lmb=2, prj_eps=1e-
         C           -- upper bound for the feasible points
         epsilon     -- coeff. used to build q (q = [epsilon - y, epsilon + y])
         eps         -- precision for the stopping condition. | direction | < eps
-        maxIter     -- max number of iterations
+        maxIter     -- max number of iterations (<= 0 for unbounded number of iterations)
         lmb0        -- initial lambda value for the projection algorithm
         d_lmb       -- initial delta_lambda value for the projection algorithm
         prj_eps     -- precision of the projection
         verbose     -- print more stats
         prj_type    -- type of projection. 1 = Goldstein, 2 = Rosen
+
     Returns:
         status      -- status of the execution. "terminated" if the program
                        has reached MaxIter. "optimal" otherwise.
@@ -297,6 +261,7 @@ def SLBQP(K, y, C, epsilon, eps=1e-6, maxIter=1000, lmb0=0, d_lmb=2, prj_eps=1e-
         v           -- minimum value of the function
     """
     
+    # Problem dimension
     n = len(y)
     
     # Input check - - - - - - - - - - -
@@ -304,32 +269,31 @@ def SLBQP(K, y, C, epsilon, eps=1e-6, maxIter=1000, lmb0=0, d_lmb=2, prj_eps=1e-
     if d1 != n or d2 != n:
         raise ValueError("Q has wrong size")
 
-    if len(y) != n:
-        raise ValueError("q has wrong size")
-
     if not isinstance(C, np.float64):
         C = np.float64(C)
+
+    if epsilon < 0:
+        raise ValueError("epsilon must be positive")
+
+    if eps < 0:
+        print("eps must be positive... replacing it with 1e-6")
+        eps = 1e-6
     # End of input check - - - - - - - - -
     
     # Initialization - - - - - - - - - - -
-    Z_old = set()
-    U_old = set()
-    i = 1
-    
     # x = [x1, x2]
-    #x1 = np.zeros(n)
-    #x2 = np.zeros(n)
     x1 = np.full(n, C/2)
     x2 = np.full(n, C/2)
     
     # q = [q1, q2]
     q1 = epsilon - y
     q2 = epsilon + y
-    # - - - - - - - - - - - - - - - - - -
-    
+    # End of initialization - - - - - - -
+
     if verbose:
-        print("Iter.\tFunction val\t||gradient||\t||direction||\t  |Z|\t  |U|\tStepsize\tMaxStep")
-    
+        print("Iter.\tFunction val\t||gradient||\t||direction||\tStepsize\tMaxStep")
+
+
     while True:
         # Compute function value (v) and gradient (g) - - -
         Kx1 = K @ x1
@@ -346,13 +310,11 @@ def SLBQP(K, y, C, epsilon, eps=1e-6, maxIter=1000, lmb0=0, d_lmb=2, prj_eps=1e-
         
         # Compute descent direction
         if prj_type == 1:
-            d1, d2 = project(x1 - g1, x2 - g2, C, lmb0, d_lmb, prj_eps)
+            d1, d2 = project_Goldstein(x1 - g1, x2 - g2, C, lmb0, d_lmb, prj_eps)
             d1 = d1 - x1
             d2 = d2 - x2
         else:
-            d1, d2 = project1(-g1, -g2, x1, x2, C, n)
-            #dg = d1 @ g1 + d2 @ g2
-            #print(f" ({dg}) ", end='')
+            d1, d2 = project_Rosen(-g1, -g2, x1, x2, C, n)
         
         # Compute the norm of the gradient (g) and of the direction (d)
         g_norm = np.sqrt((g1 @ g1) + (g2 @ g2))
@@ -361,29 +323,14 @@ def SLBQP(K, y, C, epsilon, eps=1e-6, maxIter=1000, lmb0=0, d_lmb=2, prj_eps=1e-
         # Print stats - - - - - - - - - - -
         if verbose:
             print("%5d\t%1.8e\t%1.8e\t%1.8e" % (i, v, g_norm, d_norm), end="")
-            
-#            Z, U = active_set(x, C, prj_eps)
-#            Z_changed, U_changed = active_set_changes(Z_old, U_old, Z, U)
-#
-#            if Z_changed:
-#                print("\t%5d*" % (len(Z)), end="")
-#            else:
-#                print("\t%5d" % (len(Z)), end="")
-#
-#            if U_changed:            
-#                print("\t%5d*" % (len(U)), end="")
-#            else:
-#                print("\t%5d" % (len(U)), end="")
         # - - - - - - - - - - - - - - - - -
         
         # Check for termination
         if(d_norm < eps):
-            if verbose :
-                print("")
+            if verbose : print("")
             return ('optimal', np.block([x1, x2]), v)
-        if(maxIter != -1 and i >= maxIter):
-            if verbose:
-                print("")
+        if(maxIter > 0 and i >= maxIter):
+            if verbose: print("")
             return ('terminated', np.block([x1, x2]), v)
         
         
@@ -429,6 +376,3 @@ def SLBQP(K, y, C, epsilon, eps=1e-6, maxIter=1000, lmb0=0, d_lmb=2, prj_eps=1e-
         x2 = x2 + alpha * d2
         
         i = i + 1
-#        if verbose:
-#            Z_old = Z
-#            U_old = U
